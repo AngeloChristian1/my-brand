2da8bfe78647952c4f92f06041d9cbf42577e73d
Gatete Angelo Christian <ishgatetechristian@gmail.com>
2024-03-14 10:33:06 +0200

    Ft: Integration

--- Js/dashboard.js
+++ Js/dashboard.js
@@ -1,6 +1,7 @@
 
+
 const loggedUser = JSON.parse(localStorage.getItem("loggedUser"));
-console.log("loggedUser", loggedUser);
+console.log("loggedUser form dash.js", loggedUser);
 const tokens = JSON.parse(localStorage.getItem("tokens"));
       console.log("tokens", tokens);
 
@@ -13,3 +14,6 @@ let request = new Request(`http://localhost:8080/`, {
   method: "GET",
   headers: myHeaders
 });
+
+// variables
+
